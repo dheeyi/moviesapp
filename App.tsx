cfbf18553ba5a9b98c9d679975bb7c5ea1199976
William Barra <wbarra-ext@wondrhealth.com>
2025-03-14 16:43:35 -0400

    Navigation 1

--- App.tsx
+++ App.tsx
@@ -1,27 +1,21 @@
 import React from 'react';
-import {
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/screens/Home/Home.tsx';
 
+const Stack = createNativeStackNavigator();
+
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <View style={backgroundStyle}>
-        <View>
-            <Home />
-        </View>
-    </View>
+      <NavigationContainer>
+          <Stack.Navigator>
+              <Stack.Screen
+                  name="Home"
+                  component={Home}
+                  options={{ headerShown: true }}
+              />
+          </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
