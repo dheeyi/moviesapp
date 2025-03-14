c61fe1319a6c7383f44b606c216f846cbf65b4fb
William Barra <wbarra-ext@wondrhealth.com>
2025-03-14 17:08:39 -0400

    Navigator 2

--- App.tsx
+++ App.tsx
@@ -1,20 +1,51 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './src/screens/Home/Home.tsx';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome5 from '@react-native-vector-icons/fontawesome5';
 
-const Stack = createNativeStackNavigator();
+import Home from './src/screens/Home/Home.tsx';
+import Search from './src/screens/Search.tsx';
+import WishList from './src/screens/WishList.tsx';
+import Profile from './src/screens/Profile.tsx';
+
+const Tab = createBottomTabNavigator();
+
+const iconMap : any = {
+    Home: 'house-user',
+    Search: 'search',
+    WishList: 'stream',
+    Profile: 'user-alt',
+};
 
 function App(): React.JSX.Element {
   return (
       <NavigationContainer>
-          <Stack.Navigator>
-              <Stack.Screen
+          <Tab.Navigator
+              screenOptions={({ route } : any) => ({
+                  tabBarIcon: ({ color, size }) => {
+                      const iconName = iconMap[route.name];
+                      return <FontAwesome5
+                          name={iconName}
+                          size={size}
+                          color={color}
+                          iconStyle="solid"
+                      />;
+                  },
+                  tabBarActiveTintColor: '#F2C94C',
+                  tabBarInactiveTintColor: '#FFFFFF',
+                  tabBarActiveBackgroundColor: '#000000',
+                  tabBarInactiveBackgroundColor: '#000000',
+              })}
+          >
+              <Tab.Screen
                   name="Home"
                   component={Home}
-                  options={{ headerShown: true }}
+                  options={{ headerShown: false }}
               />
-          </Stack.Navigator>
+              <Tab.Screen name="Search" component={Search} />
+              <Tab.Screen name="WishList" component={WishList} />
+              <Tab.Screen name="Profile" component={Profile} />
+          </Tab.Navigator>
       </NavigationContainer>
   );
 }
